6484653353f665c1188dfc3dcd765547430955c1
Saurabh Singh <singhsaura@microsoft.com>
2024-06-02 08:04:50 -0700

    adding the sequencer

---
+++ TestApplication/SqlXmlTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestApplication
+{
+    public  class SqlXmlTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public SqlXmlTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void TestXmlRetrieval()
+        {
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "tcp:localhost,1444";
+            builder.IntegratedSecurity = true;
+            builder.InitialCatalog = "testdatabase";
+            builder.TrustServerCertificate = true;
+            builder.Encrypt = false;
+
+            string query = "select [EmployeeDetails] from dbo.Employees";
+
+            using SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            connection.Open();
+            using SqlCommand command = connection.CreateCommand();
+            command.CommandText = query;
+            using SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                _output.WriteLine(reader.GetSqlXml(0).Value);
+            }
+        }
+    }
+}
---
+++ src/Microsoft.Data.SqlClient/netcore/src/SqlClientX/Streams/MarsSequencer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Data.SqlClient.SqlClientX.Streams
+{
+    internal class MarsSequencer
+    {
+        private int _sessionId;
+        private int _sequenceNumber;
+
+        public MarsSequencer()
+        {
+            _sequenceNumber = 0;
+            _sessionId = 0;
+        }
+
+        public int NextSequenceNumber => Interlocked.Increment(ref this._sequenceNumber);
+
+        public ushort NextSessionId
+            {
+            get {
+                ushort sessionId = _sessionId;
+                Interlocked.Increment(ref _sessionId);
+                return sessionId;
+            }
+        }
+}
